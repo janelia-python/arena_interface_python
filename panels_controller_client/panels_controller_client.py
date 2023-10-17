d6f1e75b30c1366074dc7377d7aae2926cfc4443
Peter Polidoro <peter@polidoro.io>
2023-10-17 15:25:21 -0400

    Add socket connection code

--- panels_controller_client/panels_controller_client.py
+++ panels_controller_client/panels_controller_client.py
@@ -4,11 +4,18 @@ import socket
 
 class PanelsControllerClient():
     """Python client interface to the Reiser lab PanelsController."""
-
-    def __init__(self, debug=False):
+    PORT = 62222
+    def __init__(self, address, debug=False):
         """Initialize a PanelsControllerClient instance."""
         self._debug = debug
         self._debug_print('PanelsControllerClient initializing...')
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((address, PORT))
+            except ConnectionRefusedError:
+                print(f"G4 Host doesn't appear to be running on {HOST}:{PORT}")
+
         self._debug_print('PanelsControllerClient initialized')
 
     def _debug_print(self, to_print):
